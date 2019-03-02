dc86c7fd1225974d55533d7df53c2c7ee289dd30
Tomek <whuu@gazeta.pl>
2019-03-02 07:26:48 +0100

    initial foundation projects

---
+++ src/Feature/Component/code/Controllers/ComponentController.cs
@@ -0,0 +1,17 @@
+﻿namespace LV.Feature.Component.Controllers
+{
+	using System.Web.Mvc;
+
+	public class ComponentController : Controller
+	{
+		public ComponentController()
+		{
+		}
+
+		[HttpGet]
+		public ActionResult UseSitecore()
+		{
+			return View("~/Views/Component/ScComponentView.cshtml");
+		}
+	}
+}
