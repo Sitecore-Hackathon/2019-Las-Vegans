737936112dc627ce4f31e786e63a55b1c238dd7c
Tomek <whuu@gazeta.pl>
2019-03-02 16:45:00 +0100

    Remove dependency to helix modules

--- src/Feature/CustomCortexTagger/code/Providers/CustomizableTagger.cs
+++ src/Feature/CustomCortexTagger/code/Providers/CustomizableTagger.cs
@@ -1,24 +1,50 @@
-﻿using Sitecore.ContentTagging.Core.Models;
+﻿using LV.Foundation.AI.CustomCortexTagger.Settings.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.ContentTagging.Core.Models;
 using Sitecore.ContentTagging.Core.Providers;
 using Sitecore.Data;
 using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.DependencyInjection;
+using Sitecore.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace LV.Feature.AI.CustomCortexTagger.Providers
 {
-    public class CustomizableTagger : ITagger<Sitecore.Data.Items.Item>
+    public class CustomizableTagger : ITagger<Item>
     {
-        public void TagContent(Sitecore.Data.Items.Item contentItem, IEnumerable<Tag> tags)
+        private readonly ICustomTaggerSettingService _settingsService;
+
+        public CustomizableTagger()
         {
-            var semanticField = (MultilistField)contentItem.Fields["__Semantics"];
+            _settingsService = ServiceProviderServiceExtensions.GetService<ICustomTaggerSettingService>(ServiceLocator.ServiceProvider);
+        }
+
+        public void TagContent(Item contentItem, IEnumerable<Tag> tags)
+        {
+            var tagsFieldId = _settingsService.GetCustomTaggerSettingModel().TagsFieldTarget;
+
+            if (tagsFieldId == Guid.Empty)
+            {
+                Log.Warn("CustomTagger: Tags field name not defined in settings", this);
+                return;
+            }
+
+            var tagsField = (MultilistField)contentItem.Fields[new ID(tagsFieldId)];
+            if (tagsField == null)
+            {
+                Log.Warn($"CustomTagger: Field {tagsFieldId} not found or wrong type in item {contentItem.ID}", this);
+                return;
+            }
+
             contentItem.Editing.BeginEdit();
             foreach (var tag in tags)
             {
-                if (ID.TryParse(tag.ID, out ID id) && !semanticField.TargetIDs.Contains(id))
+                if (ID.TryParse(tag.ID, out ID id) && !tagsField.TargetIDs.Contains(id))
                 {
-                    semanticField.Add(tag.ID);
+                    tagsField.Add(tag.ID);
                 }
             }
             contentItem.Editing.EndEdit();
