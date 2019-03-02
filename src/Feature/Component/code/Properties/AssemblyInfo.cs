758d65f94c1e618a7da6ad03384df10de58833d9
rdebowski <r.debowski@yahoo.com>
2019-03-02 08:00:56 +0100

    Feature project preparation

--- src/Feature/Component/code/Properties/AssemblyInfo.cs
+++ src/Feature/Component/code/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("LV.Feature.Component")]
+[assembly: AssemblyTitle("LV.Feature.GDPR.Operations")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LV.Feature.Component")]
+[assembly: AssemblyProduct("LV.Feature.GDPR.Operations")]
 [assembly: AssemblyCopyright("Copyright © Sitecore 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
